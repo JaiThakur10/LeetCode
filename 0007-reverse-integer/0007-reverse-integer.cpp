4a99ab2cc7478fbf331d4da7637447f29f79ac06
Jai Thakur <108147487+JaiThakur10@users.noreply.github.com>
2022-11-14 16:03:47 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (75.58%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,10 +3,10 @@ public:
     int reverse(int x) {
 
         int ans = 0;
-        while(x!=0){
+        while(x != 0){
             int digit = x%10;
             
-            if (ans > INT_MAX/10 || ans<INT_MIN/10){
+            if ((ans > INT_MAX/10) || (ans<INT_MIN/10)){
                 return 0;
             }
             ans = (ans*10) + digit;
